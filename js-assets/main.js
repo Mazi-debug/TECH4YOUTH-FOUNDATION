4dcb0fc938c6493b8fc3c9eae70cfe774fad4e93
Mazi-debug <ST10267734@vcconnect.edu.za>
2025-11-03 14:55:16 +0200

    Created a js file and linked it to all pages, added accordian to js file and implemented in webpages

---
+++ js-assets/main.js
@@ -0,0 +1,19 @@
+console.log("JavaScript is connected!");
+
+// Accordion
+document.querySelectorAll(".accordion-btn").forEach((btn) => { //Selects all elements with class accordion-btn.
+
+  /*Loops through each button and adds a click listener.
+On click: Toggles the class "active" (you can use this in CSS for styling open/closed buttons).
+ */
+  btn.addEventListener("click", function() {
+    btn.classList.toggle("active");
+
+    /* Selects the next element (nextElementSibling) which is the accordion content.
+Checks if it’s visible:
+If display is "block", hide it ("none")
+If hidden, show it ("block") */
+    let content = btn.nextElementSibling;
+    content.style.display = content.style.display === "block" ? "none" : "block";
+  });
+});
